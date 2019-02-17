dc5ff01982aed910345f09a85b08c331281627ea
Justin Brooks <36597246+Jbrooks1200@users.noreply.github.com>
2019-02-16 22:42:51 -0500

    added API token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiamJyb29rczEyMDAiLCJhIjoiY2pzNmNpeGd2MG94czN5cXBrN2wycmdnOCJ9.BAgeDNU1Fj9xysIqerAM4w',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
